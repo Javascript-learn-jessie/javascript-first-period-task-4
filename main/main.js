ee3d9b80ab77fb44e9806874c2169a73bce34ede
jessie-gan <jessiegan@foxmail.com>
2018-04-14 09:27:32 +0800

    calculate fee

--- main/main.js
+++ main/main.js
@@ -1,4 +1,12 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function calculateTaxiFee(mileage, park_wait_time) {
+    let fee;
+    if (mileage <= 2) {
+        fee = 6 + park_wait_time * 0.25;
+    } else if (mileage <= 8) {
+        fee = 6 + (mileage - 2) * 0.8 + park_wait_time * 0.25;
+    } else {
+        fee = 6 + (mileage - 2) * 0.8 + (mileage - 8) * 0.5 + park_wait_time * 0.25;
+    }
+    fee = Math.ceil(fee);
+    return fee;
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,29 @@
-const main = require('../main/main');
+const calculateTaxiFee = require('../main/main');
 
 describe('taxi fee', function () {
+    var mileages;
+    var park_wait_time;
     // write your tests here...
+    it('the mileages are in the based range',function(){
+        mileages=1.5;
+        park_wait_time=0;
+        var expectFee = 6;
+        var actualFee = calculateTaxiFee(mileages,park_wait_time);
+        expect(actualFee).toEqual(expectFee);
+    });
+    it('the mileages over the based range but less 8 km',function(){
+        mileages = 6;
+        park_wait_time = 3;
+        var expectFee = 10;
+        var actualFee = calculateTaxiFee(mileages,park_wait_time); 
+        expect(actualFee).toEqual(expectFee);
+    });
+    it('the mileages over 8 km',function(){
+        mileages = 10;
+        park_wait_time = 10;
+        var expectFee = 16;
+        var actualFee = calculateTaxiFee(mileages,park_wait_time); 
+        expect(actualFee).toEqual(expectFee);
+    });
+
 });
